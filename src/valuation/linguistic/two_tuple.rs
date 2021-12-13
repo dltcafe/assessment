d21a34c9ac7c5d5bad5dfb7ee5d6a1b9f8c0715f
Francisco Javier Estrella Liébana <estrella@ujaen.es>
2021-12-13 11:20:01 +0100

    TwoTuple > Unified & Unified > TwoTuple
    Signed-off-by: Francisco Javier Estrella Liébana <estrella@ujaen.es>

--- src/valuation/linguistic/two_tuple.rs
+++ src/valuation/linguistic/two_tuple.rs
@@ -1,7 +1,8 @@
 use crate::domain::Qualitative;
+use crate::fuzzy::membership::Trapezoidal;
 use crate::fuzzy::{Label, LabelMembership};
 use crate::utilities;
-use crate::valuation::Linguistic;
+use crate::valuation::{Linguistic, Unified, UnifiedError};
 use crate::Valuation;
 use std::fmt::{Display, Formatter};
 
@@ -511,5 +512,169 @@ impl<'domain, T: LabelMembership> TwoTuple<'domain, T> {
     /// ```
     pub fn domain(&self) -> &'domain Qualitative<T> {
         self.domain
+    }
+}
+
+/// Generates a Unified valuation from a TwoTuple valuation.
+///
+/// # Examples
+///
+/// ```
+/// # use assessment::qualitative_domain;
+/// # use assessment::valuation::{TwoTuple, Unified, UnifiedError};
+/// # use assessment::utilities;
+/// let domain = qualitative_domain![
+///     "a" => vec![0.0, 0.0, 0.5],
+///     "b" => vec![0.0, 0.5, 1.0],
+///     "c" => vec![0.5, 1.0, 1.0]
+/// ].unwrap();
+///
+/// let valuation = TwoTuple::new_by_label_index(&domain, 1, 0.3).unwrap();
+/// let beta = valuation.inverse_delta();
+/// let unified = Unified::try_from(valuation).unwrap();
+/// assert_eq!(*unified.measures(), vec![0.0, 0.7, 0.3]);
+/// assert!(utilities::math::approx_equal_f32(unified.chi(), beta, 5));
+/// ```
+///
+/// # Errors
+///
+/// **UnifiedError::NonBLTSDomain**: If valuation domain is a Non-BLTS domain.
+///
+/// ```
+/// # use assessment::qualitative_domain;
+/// # use assessment::valuation::{TwoTuple, Unified, UnifiedError};
+/// let domain = qualitative_domain![
+///     "a" => vec![0.0, 0.0, 0.5],
+///     "b" => vec![0.0, 0.5, 1.0]
+/// ].unwrap();
+///
+/// let valuation = TwoTuple::new_by_label_index(&domain, 0, 0.3).unwrap();
+/// assert_eq!(
+///     Unified::try_from(valuation),
+///     Err(UnifiedError::NonBLTSDomain { domain: &domain })
+/// );
+/// ```
+///
+impl<'domain> TryFrom<TwoTuple<'domain, Trapezoidal>> for Unified<'domain> {
+    type Error = UnifiedError<'domain>;
+
+    fn try_from(value: TwoTuple<'domain, Trapezoidal>) -> Result<Self, Self::Error> {
+        let mut measures: Vec<f32> = vec![0.; value.domain.cardinality()];
+        let index = value.index();
+        let alpha = value.alpha();
+        measures[index] = 1. - alpha.abs();
+        if alpha != 0. {
+            measures[if alpha > 0. { index + 1 } else { index - 1 }] = alpha.abs()
+        }
+        Unified::new(&value.domain(), measures)
+    }
+}
+
+/// Generates a Unified valuation from a &TwoTuple valuation.
+///
+/// # Examples
+///
+/// ```
+/// # use assessment::qualitative_domain;
+/// # use assessment::valuation::{TwoTuple, Unified, UnifiedError};
+/// # use assessment::utilities;
+/// let domain = qualitative_domain![
+///     "a" => vec![0.0, 0.0, 0.5],
+///     "b" => vec![0.0, 0.5, 1.0],
+///     "c" => vec![0.5, 1.0, 1.0]
+/// ].unwrap();
+///
+/// let valuation = TwoTuple::new_by_label_index(&domain, 1, 0.3).unwrap();
+/// let unified = Unified::try_from(&valuation).unwrap();
+/// assert_eq!(*unified.measures(), vec![0.0, 0.7, 0.3]);
+/// assert!(utilities::math::approx_equal_f32(unified.chi(), valuation.inverse_delta(), 5));
+/// ```
+///
+/// # Errors
+///
+/// **UnifiedError::NonBLTSDomain**: If valuation domain is a Non-BLTS domain.
+///
+/// ```
+/// # use assessment::qualitative_domain;
+/// # use assessment::valuation::{TwoTuple, Unified, UnifiedError};
+/// let domain = qualitative_domain![
+///     "a" => vec![0.0, 0.0, 0.5],
+///     "b" => vec![0.0, 0.5, 1.0]
+/// ].unwrap();
+///
+/// let valuation = TwoTuple::new_by_label_index(&domain, 0, 0.3).unwrap();
+/// assert_eq!(
+///     Unified::try_from(&valuation),
+///     Err(UnifiedError::NonBLTSDomain { domain: &domain })
+/// );
+/// ```
+///
+impl<'domain> TryFrom<&TwoTuple<'domain, Trapezoidal>> for Unified<'domain> {
+    type Error = UnifiedError<'domain>;
+
+    fn try_from(value: &TwoTuple<'domain, Trapezoidal>) -> Result<Self, Self::Error> {
+        let mut measures: Vec<f32> = vec![0.; value.domain.cardinality()];
+        let index = value.index();
+        let alpha = value.alpha();
+        measures[index] = 1. - alpha.abs();
+        if alpha != 0. {
+            measures[if alpha > 0. { index + 1 } else { index - 1 }] = alpha.abs()
+        }
+        Unified::new(&value.domain(), measures)
+    }
+}
+
+/// Generates a TwoTuple valuation from an Unified valuation.
+///
+/// # Examples
+///
+/// ```
+/// # use assessment::qualitative_domain;
+/// # use assessment::utilities;
+/// # use assessment::valuation::{TwoTuple, Unified, UnifiedError};
+/// let domain = qualitative_domain![
+///     "a" => vec![0.0, 0.0, 0.5],
+///     "b" => vec![0.0, 0.5, 1.0],
+///     "c" => vec![0.5, 1.0, 1.0]
+/// ].unwrap();
+///
+/// let valuation = Unified::new(&domain, vec![0.0, 0.7, 0.3]).unwrap();
+/// let chi = valuation.chi();
+/// let two_tuple = TwoTuple::try_from(valuation).unwrap();
+/// assert_eq!(*two_tuple.domain(), domain);
+/// assert!(utilities::math::approx_equal_f32(two_tuple.inverse_delta(), chi, 5));
+/// ```
+impl<'domain> TryFrom<Unified<'domain>> for TwoTuple<'domain, Trapezoidal> {
+    type Error = TwoTupleError<'domain, Trapezoidal>;
+
+    fn try_from(value: Unified<'domain>) -> Result<Self, Self::Error> {
+        TwoTuple::delta(value.domain(), value.chi())
+    }
+}
+
+/// Generates a TwoTuple valuation from an &Unified valuation.
+///
+/// # Examples
+///
+/// ```
+/// # use assessment::qualitative_domain;
+/// # use assessment::utilities;
+/// # use assessment::valuation::{TwoTuple, Unified, UnifiedError};
+/// let domain = qualitative_domain![
+///     "a" => vec![0.0, 0.0, 0.5],
+///     "b" => vec![0.0, 0.5, 1.0],
+///     "c" => vec![0.5, 1.0, 1.0]
+/// ].unwrap();
+///
+/// let valuation = Unified::new(&domain, vec![0.0, 0.7, 0.3]).unwrap();
+/// let two_tuple = TwoTuple::try_from(&valuation).unwrap();
+/// assert_eq!(*two_tuple.domain(), domain);
+/// assert!(utilities::math::approx_equal_f32(two_tuple.inverse_delta(), valuation.chi(), 5));
+/// ```
+impl<'domain> TryFrom<&Unified<'domain>> for TwoTuple<'domain, Trapezoidal> {
+    type Error = TwoTupleError<'domain, Trapezoidal>;
+
+    fn try_from(value: &Unified<'domain>) -> Result<Self, Self::Error> {
+        TwoTuple::delta(value.domain(), value.chi())
     }
 }
